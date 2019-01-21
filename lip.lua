ef2728b60b3e767297c8a740b5e2413fd979b1e2
Fokin <ofokin@it-pro.loc>
2019-01-21 13:37:04 +0200

    QLUA QUIK SOCKET SERVER
    QLUA QUIK SOCKET SERVER

---
+++ lip.lua
@@ -0,0 +1,40 @@
+local LIP = {};
+
+--- Returns a table containing all the data from the INI file.
+--@param fileName The name of the INI file to parse. [string]
+--@return The table containing all data from the INI file. [table]
+function LIP.load(fileName)
+	local file, error = io.open(fileName, 'r')
+	
+	if (error ~= nil) then
+		return nil
+	end
+	
+	local data = {};
+	local section;
+	for line in file:lines() do
+		local tempSection = line:match('^%[([^%[%]]+)%]$');
+		if(tempSection)then
+			section = tonumber(tempSection) and tonumber(tempSection) or tempSection;
+			data[section] = data[section] or {};
+		end
+		local param, value = line:match('^([%w|_]+)%s-=%s-(.+)$');
+		if(param and value ~= nil)then
+			if(tonumber(value))then
+				value = tonumber(value);
+			elseif(value == 'true')then
+				value = true;
+			elseif(value == 'false')then
+				value = false;
+			end
+			if(tonumber(param))then
+				param = tonumber(param);
+			end
+			data[section][param] = value;
+		end
+	end
+	file:close();
+	return data;
+end
+
+return LIP;
